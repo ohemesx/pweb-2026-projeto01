a5cc7ef13922e354fd5817fef8eb542ffa4092d4
hemerson <hemersondaniel987@gmail.com>
2026-05-30 13:48:46 -0300

    atualizações finais

--- projeto01/projeto01/settings.py
+++ projeto01/projeto01/settings.py
@@ -12,23 +12,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$mey2g%dw(q9s40ci*vvw!^lz#6ono0kx!s3b_gds+bi6cmz9n'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'site01',
@@ -70,9 +61,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'projeto01.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -80,9 +68,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -99,10 +84,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -110,10 +91,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
--- projeto01/site01/static/js/home.js
+++ projeto01/site01/static/js/home.js
@@ -1,14 +1,12 @@
 window.onload = () => {
     const vanContainer = document.querySelector(".van-container"); 
 
-    // cria os faróis
     const leftLight = document.createElement("div");
     leftLight.classList.add("headlight", "left");
 
     const rightLight = document.createElement("div");
     rightLight.classList.add("headlight", "right");
 
-    // adiciona à página
     vanContainer.appendChild(leftLight);
     vanContainer.appendChild(rightLight);
 };
